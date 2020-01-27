9edf7835949704fc91cb8fe24717b9ad67c36a85
Arjun Shome <arjunshome111@gmail.com>
2020-01-28 01:46:15 +0530

    Added Binary Search tree

--- Tree/BinaryTree/BinaryTree.py
+++ Tree/BinaryTree/BinaryTree.py
@@ -8,13 +8,32 @@ class BinaryTree:
 
     def __init__(self):
         print('***********************')
-        print('***** BINARY TREE *****')
+        print('***** BINARY/BINARY SEARCH TREE *****')
         print('***********************')
         self.root = None
+        self.cuurent = None
 
     def insert_root(self, data):
         self.root = Node(data)
 
+    # Binary Search Tree insertion implementation
+    def insert_child_bst(self, root, data):
+        child = Node(data)
+        if root.data < child.data:
+            if root.right is None:
+                root.right = child
+            else:
+                self.insert_child_bst(root.right, data)
+        else:
+            if root.left is None:
+                root.left = child
+            else:
+                self.insert_child_bst(root.left, data)
+
+
+
+
+    # Binary Tree normal implementation
     def insert_child(self, data):
         if not self.root.left:
             self.root.left = Node(data)
